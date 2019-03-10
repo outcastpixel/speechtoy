80366d10144a1136a1500544a5dab01ca4c53f43
Kevin Cox <kevin@outcastpixel.com>
2019-03-09 20:15:47 -0600

    Initial Cocoa commit.
    Initial Cocoa commit.

---
+++ macosx/speechtoy/speechtoy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  speechtoy
+//
+//  Created by Kevin Cox on 3/9/19.
+//  Copyright © 2019 Kevin Cox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macosx/speechtoy/speechtoy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  speechtoy
+//
+//  Created by Kevin Cox on 3/9/19.
+//  Copyright © 2019 Kevin Cox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
